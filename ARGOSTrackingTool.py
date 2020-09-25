b4ec499707950c180e89eaef317b398aeaa13c4f
ClaudiaMeca <claudiaeia@gmail.com>
2020-09-25 17:08:39 -0400

    Initial commit of ARGOSTrackingTool script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 17:01:55 2020
+
+@author: clauv
+"""
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Claudia Meca (cm498@duke.edu)
+# Date:   Fall 2020
+#--------------------------------------------------------------
+
